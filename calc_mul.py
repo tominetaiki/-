3d4d38b9d8528abfe34bc48fb79768846a5a0919
tominetaiki <tomine.taiki.pa@tut.jp>
2022-01-31 16:14:02 +0900

    test_cases.py commit 2, calc_mul.py commit 1

--- calc_mul.py
+++ calc_mul.py
@@ -5,11 +5,11 @@ import re
 def calc(A,B):
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
+        p = re.compile('(\d+)*')#<正規表現の変更
+        if p.fullmatch(ai) and p.fullmatch(bi):#<文字列全体のマッチング
                 a=float(ai)
                 b=float(bi)
-                if 0<a and a<b and b<1000:
+                if 0<a and a<1000 and 0<b and b<1000:#<入力の定義域の変更
                         valid=True
                 else:
                         valid=False
--- test_cases.py
+++ test_cases.py
@@ -7,6 +7,18 @@ from calc_mul import calc
 
 class TestCalc (unittest.TestCase):
 
+        def test_sample1 (self):
+                self.assertEqual (21, calc(3,7))
+
+        def test_sample2 (self):
+                self.assertEqual (-1, calc(0,150))
+
+        def test_sample3 (self):
+                self.assertEqual (-1, calc('a','b'))
+
+        def test_sample4 (self):
+                self.assertEqual (-1, calc(0.1,999))
+                
         def test_sample5 (self):
                 self.assertEqual (-1, calc(999,1000))
 
@@ -15,4 +27,3 @@ class TestCalc (unittest.TestCase):
 
         def test_sample7 (self):
                 self.assertEqual (16, calc(4,4))
-
